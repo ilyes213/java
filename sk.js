f0322277264834fbd42eaf1ad2366ebe7f3fc1c3
ilyes213 <73720224+ilyes213@users.noreply.github.com>
2020-10-30 22:52:34 +0100

    Create sk.js

---
+++ sk.js
@@ -0,0 +1,86 @@
+	//SHOP SETTING
+
+	var textread = "Lire la suite"; 
+	var viewedallpost = "Plus de messages à afficher";
+	var viewedallproduct = "Plus de produits à afficher";
+	var loadmorebtn = "Charger plus";
+	var blogname = "LES DERNIÈRES NOUVELLES";
+	var blogdescription = "Suivez les articles pour obtenir la remise et les nouveaux produits";
+	var ilyestore_notify_morethan = "<span>Il n'est pas permis d'ajouter plus de 10 produits</span>";
+	var ilyestore_label = "Product"; //Do not change	
+	var ilyestore_emptycart = "Votre panier est vide. Veuillez revenir pour en ajouter";
+	var ilyestore_pricechanged = "Un problème est survenu avec votre panier.";
+	var ilyestore_notincheckout = "<span>Votre panier contient des produits, pas dans notre livraison. Redirection vers la page de diffusion...</span>";
+	var ilyestore_refresh = "<span>Mettre à jour votre panier..</span>";
+	var ilyestore_shipping_notifi = "Les frais d'expédition ont été ajoutés";
+	var ilyestore_maxproductcanadd = "10";
+	var ilyestore_quantity_notifi = "La quantité doit être supérieure à 0";
+	var ilyestore_related =  "vous pourriez aussi aimer";
+	var readmorepostbtn = "Lire la suite";
+
+	//CHANGE BUTTON TEXT
+	//For Cash On Delivery
+	var paywithpaypal = "Proceed To Paypal";
+	var cashondelivery = "Passer la commande";
+	var banktransfer = "Direct Bank Transfer";
+	
+  	//PAYPAL SETTING:
+	var paypal_email = "ilyestore@gmail.com";
+    var paypal_successpage = "http://"+window.location.hostname+"/p/success.html";
+	var paypal_cancelpage = "http://"+window.location.hostname+"/p/cancel.html";
+	
+	//CURRENCY
+	var currency_page = "DA";
+	var language_page = "english-us"; //Keep it default. do not change.
+
+    // Add City with Shipping cost. Example: Ship to "Viet Nam - Ha noi with $2". 
+	var ilyestore_cityaddressshippingcost = [
+                                    ["01,ADRAR", 1200],		
+									["02,CHLEF", 700],
+									["03,LAGHOUAT", 900],
+									["04,OUM EL BOUAGHI", 800],
+									["05,BATNA", 800],	
+									["06,BEJAIA", 700],	
+									["07,BISKRA", 1000],									
+									["08,BECHAR", 1000],									
+									["09,BLIDA", 600],
+									["10,BOUIRA", 600],	
+									["11,TAMANRASSET", 1200],									
+									["12,TEBESSA", 1000],
+									["13,TLEMCEN", 700],									
+									["14,TIARET", 800],									
+									["15,TIZI OUZOU", 600],	
+                                    ["16,ALGER", 400],
+									["17,DJELFA", 900],									
+									["18,JIJEL", 800],									
+									["19,SETIF", 700],									
+									["20,SAIDA", 800],
+									["21,SKIKDA", 800],
+									["22,SIDI BEL ABBES", 700],								
+									["23,ANNABA", 700],									
+									["24,GUELMA", 800],									
+									["25,CONSTANTINE", 700],									
+									["26,MEDEA", 700],	
+									["27,MOSTAGHANEM ", 800],									
+									["28,MSILA", 700],
+									["29,MASCARA", 800],
+									["30,OUARGLA", 1000],									
+									["31,ORAN", 700],
+									["32,EL BAYADH", 1200],									
+									["33,ILLIZI", 1200],	
+									["34,BOURDJ BOU ARERIJ", 700],									
+									["35,BOUMERDES", 600],
+									["36,EL TAREF", 900],
+									["37,TINDOUF", 1200],	
+									["38,TISSEMSILT", 900],	
+									["39,EL OUED", 1200],
+									["40,KHENCHLA", 800],
+									["41,SOUK AHRAS", 800],	
+									["42,TIPAZA", 600],																		
+									["43,MILA", 800],									
+									["44,AIN DEFLA", 800],
+									["45,NAAMA", 1200],									
+									["46,AIN TEMOUCHENT ", 800],
+									["47,GHARDAIA", 900],									
+									["48,RELIZANE", 800],
+                                  ];
